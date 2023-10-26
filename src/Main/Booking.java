2eebcf7b04a0299912f1927a3bebcd7565787480
haha0003 <haha0003@stud.kea.dk>
2023-10-26 14:27:00 +0200

    ll

---
+++ src/Main/Booking.java
@@ -0,0 +1,70 @@
+package Main;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Booking {
+    private LocalDateTime bookingTime;
+    private LocalDateTime bookingDate;
+    private String customerName;
+    private String customerMail;
+    private ArrayList<Booking> bookings;
+    final String filename = "BookingFile";
+
+
+    Booking(String customerName, String customerMail, LocalDateTime bookingDate, LocalDateTime bookingTime){
+        this.customerName = customerName;
+        this.customerMail = customerMail;
+        this.bookingDate = bookingDate;
+        this.bookingTime = bookingTime;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerMail() {
+        return customerMail;
+    }
+
+    public LocalDateTime getBookingDate() {
+        return bookingDate;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerMail(String customerMail) {
+        this.customerMail = customerMail;
+    }
+
+    public void setBookingDate(LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public void createBooking(){
+    }
+
+    public void deleteBooking(){
+    }
+
+    public void saveFileAfterDeleteBooking(){
+    }
+
+    public void saveFile(){
+    }
+
+
+}
+
+
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,6 @@
 package Main;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("dada");
         System.out.println("lili");
