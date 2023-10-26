0c06217d0ca699e5afcfadf9fce3f216bc195052
Arfi <arma0001@stud.kea.dk>
2023-10-26 12:57:38 +0200

    update1

--- src/Main/main.java
+++ src/Main/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("dada");
         System.out.println("lili");
+        System.out.println("hanadi er sjov");
     }
 }
