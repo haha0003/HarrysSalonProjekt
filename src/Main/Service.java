e31538768fb238ae57bd17539846f8fd7850af42
haha0003 <haha0003@stud.kea.dk>
2023-10-27 12:03:07 +0200

    ll

---
+++ src/Main/Service.java
@@ -0,0 +1,26 @@
+package Main;
+
+import java.util.ArrayList;
+
+public class Service {
+
+    private String name;
+    private double price;
+    private ArrayList <Service> services = new ArrayList<>();
+
+    Service(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    Service cut = new Service("Haircut", 200);
+
+
+
+
+
+
+
+
+
+}
